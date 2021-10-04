2d945c230b6a3e27e04b8073a9b0559e0ecf632b
kamrancode <kamranparmasii@gmail.com>
2021-10-05 01:18:01 +0330

    created backend from strapi, models and controllers, updated gitignore

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ecb223a0b59db1fd96759903c888be9b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2de5812c-2c53-4db6-9dbf-a9d931721c16'
+};
